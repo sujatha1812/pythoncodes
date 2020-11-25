9280dfb9c81dd83c981df5e2290639dd925f8a0f
sujatha1812 <53338446+sujatha1812@users.noreply.github.com>
2020-11-25 21:31:51 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,25 @@
+#divide, multiply,add#
+
+a=b=c=10
+print(a/10)
+print(b*50)
+print(c+60)
+
+#string variable 5 characters & replace 3rd character with G#
+str="abcde"
+str=str[:3]+"g"+str[3+1:]
+print(str)
+
+#conversion of Typecast#
+
+a = 5
+print(a,'is of type:',type(a))
+
+a = float(a)
+print(a,'is of type:',type(a))
+
+b=10.0
+print(b,'is of type:',type(b))
+
+b=int(b)
+print(b,'is of type:',type(b))
