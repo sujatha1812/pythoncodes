88a54cbccb17c145cc1336e60e2db6126e8c7a30
sujatha1812 <53338446+sujatha1812@users.noreply.github.com>
2020-11-25 19:29:51 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,59 @@
+# To print value#
+print("30 days 30 hours challenge")
+
+# Assign string to variable#
+
+Hours="thirty"
+print(Hours)
+
+#Indexing using string#
+
+Days="thity days"
+print(Days[1])
+
+#Particular character from certain text without space#
+
+c="welcome30days30hourschallenge"
+print(c[2:10])
+
+#Particular character from certain text with space#
+
+c="welcome 30 days 30 hours challenge"
+print(c[2:15])
+
+challenge = "I will win"
+print(challenge[7:10])
+
+#length of character#
+print(len(challenge))
+
+#string conversion#
+
+print(challenge.lower())
+
+print(c.upper())
+
+#concatenation#
+
+a="30days"
+b="30hours"
+c=a+b
+print(c)
+
+#concatenation#
+a="30days"
+b="30hours"
+c=a+" "+b
+print(c)
+
+#casefold()#
+text="Thirty Days and Thirty Hours"
+x=text.casefold()
+print(x)
+
+# slice from second value to last value#
+s="helloworld"
+print(s[2:])
+---------------------------------------------------------------
+
+
